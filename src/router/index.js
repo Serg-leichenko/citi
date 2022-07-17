ac68df0a800ac2a153b9492a014b198b84c10b2d
Sergey Leichenko <mega-serg2707@ukr.net>
2022-07-17 12:07:26 +0300

    login form;task list

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import ToDo from "../views/ToDo.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/todo",
+    name: "todo",
+    component: ToDo,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
